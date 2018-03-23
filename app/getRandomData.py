0f1e6e5d76f31177682106c36e29b71a39aba760
Sam King <samking@Sams-MacBook-Air.local>
2018-03-23 18:09:52 -0400

    lib stfuffff

--- app/getRandomData.py
+++ app/getRandomData.py
@@ -1,1 +1,20 @@
-import random
+from random import randint
+
+def getRandomItems( numOfItems, factXLen, factYLen, factZLen ) :
+    #arr to return
+    pointsArr = set()
+
+    while len( pointsArr ) < numOfItems :
+        #using 0-14 since only odd ailes have shelves
+        point = ( randint(1,factXLen - 2), randint(0,factYLen - 1), randint(0,factZLen - 1) )
+        print(point[1] & 1)
+        if ( point[1] & 1 ) == 0 :
+            newPoint = point[1] + 1
+            point = ( point[0], newPoint, point[2] )
+        #point = ( point[0], point[1] * 2, point[2])
+        pointsArr.add( point )
+
+    return pointsArr
+
+
+print(getRandomItems(5,10,30,10))
