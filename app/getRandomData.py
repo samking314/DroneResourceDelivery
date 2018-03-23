0e250a17a19e04ecf246d3c05a89cdcfb97b44b2
Sam King <samking@Sams-MacBook-Air.local>
2018-03-23 18:18:58 -0400

    rando calrissian done

--- app/getRandomData.py
+++ app/getRandomData.py
@@ -5,16 +5,16 @@ def getRandomItems( numOfItems, factXLen, factYLen, factZLen ) :
     pointsArr = set()
 
     while len( pointsArr ) < numOfItems :
-        #using 0-14 since only odd ailes have shelves
+        #using 1...factXlen-2 since the first and last rows are 'highways'
         point = ( randint(1,factXLen - 2), randint(0,factYLen - 1), randint(0,factZLen - 1) )
-        print(point[1] & 1)
+
         if ( point[1] & 1 ) == 0 :
             newPoint = point[1] + 1
             point = ( point[0], newPoint, point[2] )
-        #point = ( point[0], point[1] * 2, point[2])
+
         pointsArr.add( point )
 
     return pointsArr
 
 
-print(getRandomItems(5,10,30,10))
+#print(getRandomItems(5,10,30,10))
