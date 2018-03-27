4292a5384fb6c28272398ef64f77677c56d25b60
Sam King <samking@Sams-MacBook-Air.local>
2018-03-27 11:31:38 -0400

    yeah this is research

--- app/findPaths.py
+++ app/findPaths.py
@@ -18,39 +18,93 @@ droneSet = {}
 #all open highways in the factory at a given time t
 openHighways = []
 #trajectoryArrays contains arrays of trajectories for new Drones
-trejectoryArrays = []
+trajectoryArrays = []
 #availableDrones is an array of IDs of Drones currently not working
 availableDrones = []
 
+#dims of the factory
 XLen = 10
 YLen = 30
 ZLen = 10
 
+exit = ( 10, 30, 0)
+
+MAX_TIME_TO_FINISH = 60 #max time to get item or exit
+
 #TODO: write frunction to take all Drones at given discrete time
 #and add a drone to the array of active Drones
-def findTrajectory( destination, currTime ) :
-    finalTime = currTime #initialize the final time
-    #TODO: loop through all time arrays in
+def findTrajectory( destination, desiredFinalTime, currentLocation = ( 0, 0, 10 ) ) :
+    trajectory = []
+    finalTime = desiredFinalTime
+    falseFactory = [ [ [ 0 for i in range(0,10)] for j in range(0,30)] for k in range(0,10)]
+    while trajectory[ len( trajectory ) ] != currentLocation :
+        trajectory = []
+        finalTime += 1
+        currTime = 0
+        falseFactory = recurseTraj( currentLocation, destination, currTime, finalTime, falseFactory )
+        #TODO: create func to run the false factory to find truths
+    return trajectory
+
+def recurseTraj( currentLocation, destination, currTime, finalTime, falseFactory ) :
+    #if there's no way to get there at that time
+    if ( len( trajectory ) == 0 and not ( destination in openHighways[finalTime] ) ) :
+        return falseFactory
+    #WRITE OUT CASES DUMMY
+    if ( destination in openHighways[finalTime] ) :
+        falseFactory[destination[0]][destination[1]][destination[2]] = 1
+        if ( destination[0] == currentLocation[0] and
+            destination[1] == currentLocation[1] and
+            destination[2] == currentLocation[2] ) :
+            return falseFactory
+        if ( ( destination[1] & 1 ) == 0 ) :
+            return recurseTraj( currentLocation,
+                                destination,
+                                currTime,
+                                finalTime,
+                                falseFactory )
+        else
+            return recurseTraj( currentLocation,
+                                destination,
+                                currTime,
+                                finalTime - 1,
+                                falseFactory )
+
+
+    if ( len( trajectory ) > 0 and
+        ( ( destination in openHighways[finalTime] ) or
+        ( destination in openHighways[finalTime] ) or
+        ( destination in openHighways[finalTime] )
+        ) ) :
+        falseFactory[destination[0]][destination[1]][destination[2]] = 1
+        return recurseTraj( currentLocation,
+                            destination,
+                            currTime,
+                            finalTime - 1,
+                            falseFactory )
 
 
 #TODO: write function to continuously update trajectories
 
 #TODO: write function to prevent collisions
 
-#TODO: write function to pick up item or to exit
-def newDelivery( numOfItems = 1, pointsArr = [(0,0,10)] ) :
+#finds a trajectory for drone with new delivery and updates global array data
+def newDelivery( numOfItems = 1, pointsArr = [(0,0,10)], currDrones = {}, currHighways = [], currTrajectories = [], currAvailableDrones = [] ) :
     #comment this line out after testing
     pointsArr = getRandomData.getRandomItems( numOfItems, XLen, YLen, ZLen )
+    droneSet = currDrones
+    openHighways = currHighways
+    trajectoryArrays = currTrajectories
+    availableDrones = currAvailableDrones
     #figure out trajectory of each drone
     for x in range ( 0, len(pointsArr) ) :
         trajectoryArrays.append(
                             findTrajectory(
-                                    pointsArr[x], datetime.datetime.now().time()
+                                    pointsArr[x], datetime.datetime.now().time() + MAX_TIME_TO_FINISH
                                 )
                             )
     #return the array of trajectories
-    return trajectoryArrays
+    return [ droneSet, openHighways, trajectoryArrays, availableDrones ]
 
-def getToExit( currentLocation = ( 0, 0, 10) ) :
-    #TODO: find the way to the exit
-    print("take me out!")
+#wherever you are Drone, call me and i will show you the way
+def getToExit( currentLocation = ( 0, 0, 10 ) ) :
+    return findTrajectory( (XLen, YLen, 0), datetime.datetime.now().time() + MAX_TIME_TO_FINISH )
