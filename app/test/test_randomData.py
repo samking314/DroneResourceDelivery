d9b7376f2f97f5baa603d2b17a246baba43ac0fa
sam king <samrk314@gmail.com>
2020-10-21 08:54:38 -0400

    configuring tests for jenkins

---
+++ app/test/test_randomData.py
@@ -0,0 +1,44 @@
+#
+#   testRando.py tests randomly calculated data
+#
+#   create by: Samuel King
+#
+
+import unittest
+from random import randint
+import sys
+sys.path.append('../')
+import getRandomData
+from field import Field
+
+class TestRandomData( unittest.TestCase ) :
+	
+	def createfieldfloor( self, XLen, YLen, ZLen ) :
+		pointsMatrix = getRandomData.createFieldFloor( XLen, YLen, ZLen )
+		filled = True
+		for i in range( len( pointsMatrix ) ) :
+			count = 0
+			for j in range( len( pointsMatrix[i] ) ) :
+				count += 1
+			if count != YLen :
+				filled = False
+				break
+		self.assertTrue( filled )
+
+	def createitemlocations( self, numOfItems ) :
+		field = Field()
+		locations = field.itemLocations
+		count = 0
+		for i in range( len( locations ) ) :
+			count += 1
+		self.assertEqual( count, 10 )
+
+	# test field floor for completeness
+	def test_createfieldfloor( self ) :
+		for x in range( 0, 10 ) :
+			self.createfieldfloor( 100, 100, 100 )
+
+	# test all items were created
+	def test_createitemlocations( self ) :
+		for x in range( 0, 10 ) :
+			self.createitemlocations( 10 )
